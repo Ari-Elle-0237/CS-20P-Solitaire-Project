eefbd6137aa2694477399f5460b63adead9549bd
Ari-Elle-0237 <arielzepezauer@gmail.com>
2024-10-22 15:22:59 -0700

    Added Starter Code

---
+++ color.py
@@ -0,0 +1,24 @@
+# color.py
+# Steve J. Hodges, Cabrillo College, sthodges@cabrillo.edu
+# first test of colors; uses tput-ncurses-bash
+
+
+import os
+
+WHITE = 7
+BLACK = 0
+GREEN = 2
+RED = 1
+
+def color(n):
+    os.system(f"tput setaf {n}")
+
+def fgcolor(n):
+    color(n)
+
+def bgcolor(n):
+    os.system(f"tput setab {n}")
+
+def nocolor(n):
+    os.system("tput sgr0")
+    
