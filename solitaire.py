ee616e8f74ca5791881db1b3065f2b272bfe4dcc
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-10-22 15:35:42 -0700

    Create solitaire.py

---
+++ solitaire.py
@@ -0,0 +1,16 @@
+"""
+Assignment #5: Solitaire
+integer_set.py
+by Ariel Zepezauer (arielzepezauer@gmail.com) and Luna Raggio (Aidanpraggio@gmail.com)
+Pengo: 'azepezau' & 'araggio'
+Test Cases in unittest_.py
+Repository at: https://github.com/Ari-Elle-0237/CS-20P-Solitaire-Project.git
+Due: Oct 28th 2024
+Exit Code: _
+"""
+
+import cards
+import color
+
+class GameBoard:
+    
