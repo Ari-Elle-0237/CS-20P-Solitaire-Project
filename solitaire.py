39b755effbb7f9b6c0a8784fe8d741062fc92001
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-10-22 15:57:39 -0700

    Started building skeleton

--- solitaire.py
+++ solitaire.py
@@ -5,7 +5,7 @@ by Ariel Zepezauer (arielzepezauer@gmail.com) and Luna Raggio (Aidanpraggio@gmai
 Pengo: 'azepezau' & 'araggio'
 Test Cases in unittest_.py
 Repository at: https://github.com/Ari-Elle-0237/CS-20P-Solitaire-Project.git
-Due: Oct 28th 2024
+Due: Nov 28th 2024
 Exit Code: _
 """
 
@@ -16,4 +16,29 @@ class GameBoard:
     # TODO:
     # - Store the deck
     # - Store State of board
-    # - Functions for Different
+    # - Functions for Different game actions
+    # - Win detection
+    def __init__(self):
+        self.deck = [Card(rank, suit) for rank, suit in Card.get_varieties()]
+        self.tableaus = [[] for _ in range(4)]
+        self.columns =  [[] for _ in range(6)]
+        cards.shuffle(self.deck)
+
+
+    @property
+    def board(self):
+        return self._board
+
+    @board.setter
+    def board(self, value):
+
+
+
+class Card:
+    def __init__(self, rank, suit):
+        pass
+
+    @classmethod
+    def get_varieties(cls):
+        return []
+
