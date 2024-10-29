f5612b1e3ee0815b5042fdedd44e43b77caf9ec9
Ari-Elle-0237 <88022155+Ari-Elle-0237@users.noreply.github.com>
2024-10-29 14:04:20 -0700

    Create unittest_solitaire.py

---
+++ unittest_solitaire.py
@@ -0,0 +1,13 @@
+import unittest
+import solitaire as sol
+
+class MyTestCase(unittest.TestCase): # add assertion here
+    def test_deal_cards(self):
+        pass
+
+    def test_can_flip_cards(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
