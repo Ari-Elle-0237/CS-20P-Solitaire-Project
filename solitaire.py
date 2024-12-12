8babd08b8c7c1eb8f5e46144e3c682510c0bc613
Games-Bookstore <Crazy.dyamond69@gmail.com>
2024-12-11 19:52:59 -0800

    Changed my name to uhh err be safer haha

--- solitaire.py
+++ solitaire.py
@@ -1,7 +1,7 @@
 """
 Assignment #5: Solitaire
 integer_set.py
-by Ariel Zepezauer (arielzepezauer@gmail.com) and Luna Raggio (Aidanpraggio@gmail.com)
+by Ariel Zepezauer (arielzepezauer@gmail.com) and Aidan Raggio (Aidanpraggio@gmail.com)
 Pengo: 'azepezau' & 'araggio'
 Test Cases in unittest_.py
 Repository at: https://github.com/Ari-Elle-0237/CS-20P-Solitaire-Project.git
